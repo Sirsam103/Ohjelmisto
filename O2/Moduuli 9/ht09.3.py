8502d81a1bcdd4b6aac51c7de21ed1a73a14d060
Sirsam103 <samuelpuhakka@gmail.com>
2023-10-24 19:31:56 +0300

    commit

--- O2/Moduuli 9/ht09.3.py
+++ O2/Moduuli 9/ht09.3.py
@@ -5,7 +5,7 @@ class Auto:
         self.regis = input_regis
         self.velmax = input_velmax
         self.velnow = 60
-        self.dist = 0
+        self.dist = 2000
     def accelerate(self, input_velchange):
         self.velnow += input_velchange
         if (self.velnow > self.velmax):
@@ -15,24 +15,6 @@ class Auto:
     def move (self, input_timeused):
         self.dist += self.velnow * input_timeused
 
-
-auto_lista = []
-for i in range(1, 11):
-    auto = Auto(f"ABC-{i}", random.randint(100, 200))
-    auto_lista.append(auto)
-
-runs = 0
-while True:
-    for auto in auto_lista:
-        auto.accelerate(random.randint(-10, 15))
-        auto.move(1)
-        #print(f'{auto.regis}, {auto.velnow}, {auto.dist}, {runs}')
-        if auto.dist >= 10000:
-            print(f'''Auto {auto.regis} voitti kilpailun!!!
-Kilpailussa kesti {runs} tuntia\n
-Autojen ominaisuudet ovat:
-Rekisteritunnus, kuljettu matka, huippunopeus, tämänhetkinen nopeus.''')
-            for auto in auto_lista:
-                print(f'{auto.regis, auto.dist, auto.velmax, auto.velnow}')
-            exit(0)
-    runs += 1
+auto = Auto(f"ABC-123", 142)
+auto.move(1.5)
+print(auto.dist)
