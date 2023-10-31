a20065c786648fd4b3aaf34e09ffa86bcd1195fa
Sirsam103 <samuelpuhakka@gmail.com>
2023-10-31 14:15:18 +0200

    assignment

--- O2/Moduuli 11/ht11.2.py
+++ O2/Moduuli 11/ht11.2.py
@@ -1,19 +1,4 @@
-class Car:
-    def __init__(self, input_regis, input_velmax, input_velnow):
-        self.regis = input_regis
-        self.velmax = input_velmax
-        self.velnow = input_velnow
-        self.dist = 0
-
-    def accelerate(self, input_velchange):
-        self.velnow += input_velchange
-        if self.velnow > self.velmax:
-            self.velnow = self.velmax
-        elif self.velnow < 0:
-            self.velnow = 0
-
-    def move(self, input_timeused):
-        self.dist += self.velnow * input_timeused
+from auto import Car
 
 
 class Gas(Car):
