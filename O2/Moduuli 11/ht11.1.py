9eb86a29abb10badc8366777ce58cf1bc7c5b873
Sirsam103 <samuelpuhakka@gmail.com>
2023-10-31 13:53:10 +0200

    assignment

--- O2/Moduuli 11/ht11.1.py
+++ O2/Moduuli 11/ht11.1.py
@@ -1,23 +1,25 @@
 class Publication:
     def __init__(self, input_name):
         self.name = input_name
-    def print(self):
-        print(f'Julkaisun nimi: {self.name}')
+
+
 class Book(Publication):
     def __init__(self, input_name,  input_author, input_pages):
         super().__init__(input_name)
         self.author = input_author
         self.pages = input_pages
+
     def print(self):
-        super().print()
-        print(f'Kirjoittaja: {self.author}\nSivumäärä: {self.pages}\n')
+        print(f'Kirjan nimi: {self.name}\nKirjoittaja: {self.author}\nSivumäärä: {self.pages}\n')
+
+
 class Comic(Publication):
     def __init__(self, input_name, input_publisher):
         super().__init__(input_name)
         self.publisher = input_publisher
+
     def print(self):
-        super().print()
-        print(f'Päätoimittaja: {self.publisher}\n')
+        print(f'Lehden nimi: {self.name}\nPäätoimittaja: {self.publisher}\n')
 
 
 aku_ankka = Comic("Aku Ankka", "Aki Hyyppä")
