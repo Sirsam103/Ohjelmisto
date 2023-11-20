4f5328605a9cb82e44c7ed4931285f63facf3af9
Sirsam103 <samuelpuhakka@gmail.com>
2023-11-20 23:50:30 +0200

    assignment

--- O2/JS Moduuli 3/t3/3.js
+++ O2/JS Moduuli 3/t3/3.js
@@ -1,2 +1,7 @@
-'use strict';
 const names = ['John', 'Paul', 'Jones'];
+const target = document.getElementById('target');
+let list = ''
+for (let i = 0; i< names.length; i++){
+  list += `<li>${names[i]}</li>`;
+}
+target.innerHTML = list;
