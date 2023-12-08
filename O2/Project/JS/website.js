4cc5066e7a3f7a6b8929fd964cd6840131a8ffcc
Sirsam103 <samuelpuhakka@gmail.com>
2023-12-08 02:07:55 +0200

    project

--- O2/Project/JS/website.js
+++ O2/Project/JS/website.js
@@ -1,3 +1,41 @@
+/*----Start of progress tab fish data----*/
+async function fetchFishData() {
+    try {
+        const response = await fetch('JSON/SeOnVittuKALA.json');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching fish data:', error);
+    }
+}
+async function displayFishData() {
+    const fishData = await fetchFishData();
+    const progressContent = document.querySelector('#proBox .secondaryBoxContent');
+    fishData.forEach(fish => {
+        const fishElement = document.createElement('div');
+        fishElement.classList.add('fishItem');
+        fishElement.style.width = '5rem';
+        const fishName = document.createElement('h3');
+        fishName.textContent = fish.name;
+        const fishLink = document.createElement('a');
+        fishLink.href = fish.url;
+        fishLink.textContent = "Learn more";
+        const fishImage = document.createElement('img');
+        fishImage.src = fish.img_src_set['1.5x'];
+        fishElement.appendChild(fishName);
+        fishElement.appendChild(fishImage);
+        fishElement.appendChild(fishLink);
+        progressContent.appendChild(fishElement);
+    });
+}
+document.addEventListener('DOMContentLoaded', () => {
+    displayFishData();
+});
+/*----End of progress tab fish data----*/
+
+
+
+/*----Start of button event listener----*/
 const buttons = document.querySelectorAll('.navButton');
 const closeButtons = document.querySelectorAll('.closeButton');
 const overlay = document.getElementById('overlay');
@@ -15,3 +53,4 @@ closeButtons.forEach(closeButton => {
 		this.closest('.secondaryBox').style.display = 'none';
 	});
 });
+/*----End of button event listener----*/
